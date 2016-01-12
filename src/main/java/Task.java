33d8a2e8f9c712b18d9d3f49eb826d6f28fb7196
Chris Houghton and Nathan Romike <cehoughton71@gmail.com>
2016-01-12 09:09:16 -0800

    Add first passing test

---
+++ src/main/java/Task.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+import java.util.HashMap;
+import javax.swing.JOptionPane;
+
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class Task {
+
+  public Task(String description) {
+
+  }
+}
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,7 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 
-public class AppTest {
+public class TaskTest {
 
   @Test
   public void Task_instantiatesCorrectly_true() {
